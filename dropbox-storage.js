00fbb7ce9785e11ca0a30e40496cd20bd2906c55
Karak <karak97@gmail.com>
2012-02-20 23:57:13 +0900

    use Dropbox as remote storage

---
+++ dropbox-storage.js
@@ -0,0 +1,31 @@
+﻿function DropboxStorage () {
+    var KEY = "<YOUR KEY>";
+    var SECRET = "<YOUR SECRET>";
+
+    var dropbox = new Dropbox(KEY, SECRET);
+
+    this.connect = function (fn) {
+        dropbox.authorize(function () { fn.apply(this, []); });
+    };
+
+    this.readDirectory = function (path, fn) {
+        dropbox.getDirectoryContents(
+            path.toString(),
+            function (data, status, xhr) {
+                var paths = [];
+                $.each(data.contents, function () {
+                    if (this.mime_type.match(/text\/plain/)) {
+                        paths.push(this.path);
+                    }
+                });
+                fn(paths);
+            }
+        );
+    };
+
+    this.readFile = function (path, fn) {
+        dropbox.getFileContents(path, function (content) {
+            fn(content);
+        });
+    };
+}
